b72279d85b5b87736d9831a0c6a45c010299343b
suxiaoxiaomm <coco_lxb@163.com>
2021-12-11 17:37:39 +0800

    modeule3

--- js/ratings.js
+++ js/ratings.js
@@ -1,5 +1,5 @@
 function collect_ratings(){
-    let ratings = {
+    const ratings = {
         count:0,
         sum:0,
         average:0,
@@ -15,8 +15,14 @@ function collect_ratings(){
 });
 
 if (ratings.count !== 0) {
-    ratings.avarage = ratings.sum /ratings.count;
+    ratings.average = ratings.sum /ratings.count;
 }
+return ratings;
 
 };
 
+document.addEventListener('change',()=>{
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+
+})
